64ebec0166265c384b514204774644e68cf124d6
Zac H <zac.haluza@gmail.com>
2020-08-06 21:50:05 -0400

    Added working progress bar & logo display logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import mandoVideo from './assets/mandalorian.mp4';
 import './sass/app.scss';
 
 const App = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(null);
+  const [showLogo, setShowLogo] = useState(false);
   const videoRef = useRef(null);
+  const progressRef = useRef(null);
+  console.log(videoRef);
 
+  const handleProgress = (currentTime, videoDuration) => {
+    // setVideoProgress((currentTime / videoDuration) * 100);
+    const progress = (currentTime / videoDuration) * 100;
+    setCurrentTime(currentTime);
+    if (!duration) setDuration(videoDuration);
+    progressRef.current.style.flexBasis = `${progress}%`;
+  };
   const togglePlay = (video) => {
     const toggle = video.paused ? 'play' : 'pause';
     video[toggle]();
+    // handleProgress(video);
     setIsPlaying((prevState) => !prevState);
   };
+
+  const handleVolume = (video, value) => {
+    video.volume = value;
+  };
+
+  useEffect(() => {
+    if (currentTime <= 3 || duration - currentTime <= 3) {
+      setShowLogo(true);
+    } else setShowLogo(false);
+  }, [currentTime]);
+
   return (
     <div className="app">
       <div className="video__container">
-        <video className="video__player" src={mandoVideo} ref={videoRef}></video>
+        <video
+          className={`video__player ${showLogo ? 'show-logo' : ''}`}
+          src={mandoVideo}
+          ref={videoRef}
+          onTimeUpdate={(e) => {
+            console.log(e.target.currentTime);
+            handleProgress(e.target.currentTime, e.target.duration);
+          }}
+        ></video>
+        {showLogo && <p>Show Logo</p>}
         <div className="video__controls">
           <div className="progress">
             <div className="progress__bar">
-              <div className="bar__filled"></div>
+              <div className="bar__filled" ref={progressRef}></div>
             </div>
             <div className="progress__time"></div>
           </div>
@@ -35,6 +68,7 @@ const App = () => {
               min="0"
               max="1"
               step=".1"
+              onChange={(e) => handleVolume(videoRef.current, e.target.value)}
             ></input>
           </div>
         </div>
